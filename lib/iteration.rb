4956237a547f645ca1c1fe60ff6e51ec9edd1998
Miriam Grigsby <miriamgrigsby@gmail.com>
2019-11-02 23:02:02 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,53 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+   inner_results = []
+ row_index = 0 
+ while row_index < src.count do
+   element_index = 0 
+   while element_index < src[row_index].count do
+  if element_index == 0
+    stringy = "I love " + src[row_index][element_index]
+  else 
+    stringy = stringy + " and " + src[row_index][element_index]
+    if element_index == src[row_index].count-1
+      inner_results.push(stringy + " on my pizza")
+    end
+  end
+     element_index += 1
+  end
+  row_index += 1
+end 
+p inner_results
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
-end
+  inner_results = []
+ row_index = 0 
+ while row_index < src.count do
+  inner_results.push(src[row_index].max)
+  row_index += 1
+  end
+  p inner_results
+end 
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+ inner_results = 0
+ row_index = 0 
+ while row_index < src.count do
+  element_index = 0 
+  while element_index < src[row_index].count do
+    if src[row_index][element_index + 1]
+      next_element = element_index + 1
+    end
+    if src[row_index][element_index].even? && element_index == 0
+      if src[row_index][next_element].even?
+        inner_results += src[row_index][element_index] + src[row_index][next_element]
+      end
+    else 
+      break
+    end
+    element_index += 1
+  end
+  row_index += 1
+end
+p inner_results
 end

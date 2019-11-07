4201560833677aa7bd0da2096dfab633533059e3
Finn Thye <piqueen@gmail.com>
2019-11-07 09:13:42 -0700

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -8,6 +8,16 @@ def join_ingredients(src)
   # As such, there should be a new String for each inner array, or pair
 end
 
+row_index = 0
+while row_index < spice_rack.count do
+  element_index = 0
+  while element_index < spice_rack[row_index].count do
+    puts spice_rack[row_index][element_index]
+    element_index += 1
+  end
+  row_index += 1
+end #=> nil
+
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
